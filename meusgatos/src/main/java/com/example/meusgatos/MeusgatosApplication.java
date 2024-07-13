93f16a8a27d1abfb209fc5904c0d812fc86e8485
LinusKienenNeto <https://github.com/LinusKienenNeto/API-GASTOS-V1.git>
2024-07-13 13:14:49 -0300

    Criado a base do projeto e criado a arvore de arquivos

---
+++ meusgatos/src/main/java/com/example/meusgatos/MeusgatosApplication.java
@@ -0,0 +1,13 @@
+package com.example.meusgatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusgatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeusgatosApplication.class, args);
+	}
+
+}
---
+++ meusgatos/src/main/java/com/example/meusgatos/domain/model/Titulo.java
@@ -0,0 +1,5 @@
+package com.example.meusgatos.domain.model;
+
+public class Titulo {
+    
+}
