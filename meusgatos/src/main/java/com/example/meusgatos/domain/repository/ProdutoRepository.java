2402559242e5cc8ba71c5841907590525ed8d6d5
LinusKienenNeto <https://github.com/LinusKienenNeto/API-GASTOS-V1.git>
2024-08-11 13:27:00 -0300

    Criado um repositorio pra produto, precisa testar

--- meusgatos/src/main/java/com/example/meusgatos/domain/repository/ProdutoRepository.java
+++ meusgatos/src/main/java/com/example/meusgatos/domain/repository/ProdutoRepository.java
@@ -1,11 +1,14 @@
 package com.example.meusgatos.domain.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.meusgatos.domain.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    List<Produto> findById(Long id);
+    
+   //eu acho que esse Optional vai dar problema.
+    @SuppressWarnings("null")
+    Optional<Produto> findById(Long id);
 }
---
+++ meusgatos/src/main/java/com/example/meusgatos/domain/service/ProdutoService.java
@@ -0,0 +1,14 @@
+package com.example.meusgatos.domain.service;
+
+import java.util.List;
+
+public interface ProdutoService<Req, Res> {
+
+    List<Res> obterTodos();
+
+    Res obterPorId(Long id);
+
+    Res cadastrar(Req id, Req dto);
+
+    void deletar(Long id);
+}
